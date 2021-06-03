57aed743adf484b0404dea5782b27ddb1aa3278b
Caroline Tan <carolinectan3@gmail.com>
2021-06-02 22:32:42 -0400

    Refactor double quotes to single

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require "./lib/game"
+require './lib/game'
 
 RSpec.describe Game do
   before :each do
@@ -7,17 +7,17 @@ RSpec.describe Game do
     @mega_millions = Game.new('Mega Millions', 5, true)
   end
 
-  it "exists" do
+  it 'exists' do
     expect(@pick_4).to be_a(Game)
     expect(@mega_millions).to be_a(Game)
   end
 
-  it "initializes with attributes" do
-    expect(@mega_millions.name).to eq("Mega Millions")
+  it 'initializes with attributes' do
+    expect(@mega_millions.name).to eq('Mega Millions')
     expect(@mega_millions.cost).to eq(5)
   end
 
-  it "can determine if it is a national drawing" do
+  it 'can determine if it is a national drawing' do
     expect(@mega_millions.national_drawing?).to eq(true)
     expect(@pick_4.national_drawing?).to eq(false)
   end
